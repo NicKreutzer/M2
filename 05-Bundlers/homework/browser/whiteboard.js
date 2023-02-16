52bb827fe707c404f078bcf511131de1da00a1a6
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-02-16 19:26:32 -0300

    Bundlers terminado

--- 05-Bundlers/homework/browser/whiteboard.js
+++ 05-Bundlers/homework/browser/whiteboard.js
@@ -1,6 +1,6 @@
-(function () {
+const EventEmitter = require('./event-emitter')
 
-  window.whiteboard = new window.EventEmitter();
+const whiteboard = new EventEmitter();
 
   // Ultimately, the color of our stroke;
   var color;
@@ -113,4 +113,4 @@
 
   };
 
-})();
+module.exports = whiteboard;
