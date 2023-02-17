9bbc504b59c5ffa359c340dca73c4e5b0a81d21c
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-02-17 18:43:46 -0300

    Primer diseño

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
-const studentName = "";
+const studentName = "Nicolas Exequiel Kreutzer.";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
+
+
 export default function Bienvenido() {
-  // el código de tu componente acá
-  return;
+ 
+  return(
+    <div>
+      <h1><strong><u>Primer diseño.</u></strong></h1>
+      <br></br>
+      <fieldset>
+      <h3><em><u>{studentName}</u></em></h3>
+      <br></br>
+      <ul>{techSkills.map((skill, index) => ( /*Iteramos sobre los elementos del array techSkills y Retornamos un elemento li por cada Skill */
+          <li key={index}>{skill}</li>        /*Con su contenido correspondiente y clave unica definida por el indice del elemento en el array */
+        ))}</ul>                           
+      </fieldset>
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
