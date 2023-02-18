d7fa558bc9d4b8f5e3db319cbc65eaf66f15f3de
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-02-18 18:53:07 -0300

    Retoque

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+const width1 = {    //! Se ajusta el fieldset a las imagenes, pero las corre a la derecha.
+   width: 200,
+   background: "green"   //! Le pone color al fieldset.
+   
+};
+
 export default function Card({name, species, gender, image, onClose}) {
    return (
       
          <div>
-            <fieldset> 
+            <fieldset style={width1}> 
             <button onClick={onClose}>X</button>
             <h1><strong>Name: {name}</strong></h1>
             <h2>Species: {species}</h2>
