03c098e793e7b0ebcaf58f77b7293af349099d70
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-02-22 18:44:54 -0300

    08 Terminado

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function Species () {
+function Species ({species, handleSpecies, handleAllSpecies}) {
+
   return (
     <div>
-
+        <h2>Species</h2>
+        {
+          species.map((especie, index)=>{
+            return(
+              <button key={index} onClick={handleSpecies} value={especie}>{especie}</button>
+            )
+          })
+        }
+        <button onClick={handleAllSpecies}>All Animals</button>
     </div>
   )
 }
+
+export default Species;
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -5,11 +5,65 @@ import Animals from '../Animals/Animals';
 import Species from '../Species/Species';
 import './Zoo.module.css';
 
+
+
 export default function Zoo() {
-   /* Escribe acá tu código */
+
+   const [zoo, setZoo] = React.useState ({
+      zooName: '',
+      animals: [],
+      species: [],
+      allAnimals: []
+   })
+
+   const handleInpuntChange = (event) =>{
+      setZoo({
+         ...zoo,    // -Spread operator, copia el valor para no perderlo.
+         zooName: event.target.value,
+      })
+   };
+
+   React.useEffect(() => {
+      fetch('http://localhost:3001/zoo')
+      .then((res) => res.json())
+      .then((data) =>
+            setZoo({
+                  ...zoo,
+                  animals: data.animals,
+                  species: data.species,
+                  allAnimals: data.animals,
+      })
+   )
+   .catch((error) => console.log(error));
+   }, []);
+
+   const handleSpecies= (event)=>{
+      const specie = event.target.value;
+      setZoo({
+         ...zoo,
+         animals: zoo.allAnimals.filter(animal => animal.specie === specie)
+      })
+   };
+
+   const handleAllSpecies = (event)=>{
+      setZoo({
+         ...zoo,
+         animals: zoo.allAnimals
+      })
+   }
+
    return (
       <div>
-         <h1>-</h1>
+         <label>Zoo Name: </label>
+         <input value= {zoo.zooName} onChange={handleInpuntChange}></input>
+         <h1>{zoo.zooName}</h1>
+
+         <Species 
+            species={zoo.species}
+            handleSpecies={handleSpecies}
+            handleAllSpecies={handleAllSpecies}/>
+
+         <Animals animals={zoo.animals}/>
       </div>
    );
 }
