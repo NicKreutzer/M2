3c4dea7467ebc8a5fd1fecd269c705ddd612df06
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2023-03-01 18:16:40 -0300

    01

--- 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
+++ 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 
 const ContactUs = () => {
+
+  const [form, setForm] = React.useState({
+    nombre: "",
+    email: "",
+    asunto: "",
+    mensaje: "",
+  });
+
+  const handleInput= (event) => {
+    setForm({
+      ...form,
+      [event.target.name] : event.target.value,
+      [event.target.email] : event.target.value,
+      [event.target.asunto] : event.target.value,
+      [event.target.mensaje] : event.target.value
+    })
+  }
+
+
   return (
     <div>
       <form className="contactBg">
         <label htmlFor="nombre">Nombre: </label>
-        <input name="nombre" />
-        <label htmlFor="email">Email: </label>
-        <input name="email" />
-        <label htmlFor="asunto">Asunto: </label>
-        <input name="asunto" />
-        <label htmlFor="mensaje">Mensaje: </label>
-        <input name="mensaje" />
+        <input onChange={handleInput} name="nombre" value= {form.nombre}/>
+        <label htmlFor="email" >Email: </label>
+        <input onChange={handleInput} name="email" value= {form.email}/>
+        <label htmlFor="asunto" >Asunto: </label>
+        <input onChange={handleInput} name="asunto" value= {form.asunto}/>
+        <label htmlFor="mensaje" >Mensaje: </label>
+        <input onChange={handleInput} name="mensaje" value= {form.mensaje}/>
         <button>Enviar</button>
       </form>
     </div>
